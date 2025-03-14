eeb920939df02c28470c3258614d59f922f18388
LValenciaga <lucianov81@gmail.com>
2025-03-13 21:19:20 -0300

    Login

--- Sistema/ventas/views.py
+++ Sistema/ventas/views.py
@@ -285,20 +285,38 @@ def add_venta_view(request):
 
 
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
-            return redirect('Index')  # Redirige a la página principal
-        else:
-            messages.error(request, "Credenciales incorrectas.")
-    return render(request, "login.html")
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('Index')
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos')
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
 
 def logout_view(request):
     auth_logout(request)
-    return redirect('Login')  # Redirige a la página de login
+    return redirect('Login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta creada para {username}. Ahora puedes iniciar sesión.')
+            return redirect('Login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
 
 
 
